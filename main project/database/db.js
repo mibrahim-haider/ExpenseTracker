d1c51e4f9eae19ad8c76249c8f92fa2342273051
mibrahim-haider <bc240419885mih@vu.edu.pk>
2025-01-14 03:06:42 +0500

    create env db.js
    env file data

--- main project/database/db.js
+++ main project/database/db.js
@@ -1,15 +1,20 @@
+require('dotenv').config();
+
 const mysql = require('mysql2');
 
-// MySQL database connection
+// MySQL database connection using environment variables
 const db = mysql.createConnection({
-  host: 'sql12.freesqldatabase.com',
-  user: 'sql12757427',
-  password: 'L2WJTUVVtU', // Replace with your MySQL root password
-  database: 'sql12757427',
+  host: `${process.env.DB_HOST}`,        // Get host from environment variable
+  user: `${process.env.DB_USER}`,        // Get user from environment variable
+  password: `${process.env.DB_PASSWORD}`, // Get password from environment variable
+  database: `${process.env.DB_NAME}`,     // Get database name from environment variable
 });
 
 db.connect((err) => {
-  if (err) throw err;
+  if (err) {
+    console.error('Error connecting to MySQL:', err.stack);
+    return;
+  }
   console.log('Connected to MySQL database.');
 });
 
